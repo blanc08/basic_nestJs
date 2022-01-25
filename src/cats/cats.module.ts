a938083f953cb418e4bcfbadc8cf9adf2975c7a7
blanc20 <bagusoktaviadi1@gmail.com>
2022-01-25 16:12:55 +0700

    modules fix

--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -1,12 +1,10 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { CatsController } from './cats.controller';
 import { CatsService } from './cats.service';
 import { CatsResolver } from './cats.resolver';
 import { Cat } from './entities/cat.entity';
 import { UsersModule } from 'src/users/users.module';
 @Module({
-  controllers: [CatsController],
   providers: [CatsService, CatsResolver],
   exports: [CatsService],
   imports: [TypeOrmModule.forFeature([Cat]), forwardRef(() => UsersModule)],
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,6 +1,5 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { UsersResolver } from './users.resolver';
 import { User } from './entities/user.entity';
@@ -8,7 +7,6 @@ import { CatsModule } from 'src/cats/cats.module';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User]), forwardRef(() => CatsModule)],
-  controllers: [UsersController],
   providers: [UsersService, UsersResolver],
   exports: [UsersService],
 })
