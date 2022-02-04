ea90024df83166163cea524f03e73fdadf918dae
blanc20 <bagusoktaviadi1@gmail.com>
2022-02-04 16:32:29 +0700

    Auth Resolver Unit Test

--- src/auth/auth.resolver.spec.ts
+++ src/auth/auth.resolver.spec.ts
@@ -1,7 +1,10 @@
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { Test, TestingModule } from '@nestjs/testing';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { getConnectionToken } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
+import { AppModule } from '../app.module';
+import { User } from '../users/entities/user.entity';
 import { UsersModule } from '../users/users.module';
 import { AuthResolver } from './auth.resolver';
 import { AuthService } from './auth.service';
@@ -10,20 +13,12 @@ import { LocalStrategy } from './local.strategy';
 
 describe('AuthResolver', () => {
   let resolver: AuthResolver;
+  let connection: Connection;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [
-        TypeOrmModule.forRoot({
-          type: 'mysql',
-          host: 'localhost',
-          port: 3306,
-          username: 'root',
-          password: '12345678',
-          database: 'learn_nestjs',
-          synchronize: true,
-          autoLoadEntities: true,
-        }),
+        AppModule,
         PassportModule,
         UsersModule,
         JwtModule.register({
@@ -35,9 +30,42 @@ describe('AuthResolver', () => {
     }).compile();
 
     resolver = module.get<AuthResolver>(AuthResolver);
+    connection = await module.get(getConnectionToken());
+  });
+
+  afterEach(() => {
+    return connection.close();
   });
 
   it('should be defined', () => {
     expect(resolver).toBeDefined();
+  });
+
+  // sign in
+  describe('signIn', () => {
+    it('should return a token', async () => {
+      jest.spyOn(resolver, 'signin').mockResolvedValue({
+        access_token: 'token',
+        user: new User(),
+      });
+
+      expect(
+        await resolver.signin(
+          { username: 'test', password: 'test' },
+          'context',
+        ),
+      ).toEqual(expect.objectContaining({}));
+    });
+  });
+
+  // sign up
+  describe('signUp', () => {
+    it('should return a token', async () => {
+      jest.spyOn(resolver, 'signup').mockResolvedValue(new User());
+
+      expect(
+        await resolver.signup({ username: 'test', password: 'test' }),
+      ).toEqual(expect.objectContaining({}));
+    });
   });
 });
